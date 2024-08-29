5c23400773dc068ac6042dad9fff929f91f1189c
Ayush Vatsal <ayushvatsal@Ayushs-MacBook-Pro.local>
2024-08-29 19:10:44 +0530

    programs and main blogs with intial data

---
+++ scripts.js
@@ -0,0 +1,28 @@
+
+// Function to handle clicks and redirect to the appropriate page
+document.getElementById("twenty-lakh-placement").onclick = function() {
+    window.location.href = "twenty-lakh-placement.html"; 
+};
+
+document.getElementById("side-hutle").onclick = function() {
+    window.location.href = "side-hustle.html"; 
+};
+
+document.getElementById("lanch-your-business").onclick = function() {
+    window.location.href = "scale-your-business.html"; 
+};
+
+
+document.getElementById("blog1").onclick = function() {
+    window.location.href = "blogs/blog1.html"; 
+};
+
+
+document.getElementById("blog2").onclick = function() {
+    window.location.href = "blogs/blog2.html"; 
+};
+
+
+document.getElementById("blog3").onclick = function() {
+    window.location.href = "blogs/blog3.html";
+};
